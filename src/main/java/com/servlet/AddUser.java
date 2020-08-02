57e0b95d1224bb1c57ef57323c3edd229b4d960f
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-08-02 17:44:52 -0300

    chore: remove unnecessary code

--- src/main/java/com/servlet/AddUser.java
+++ src/main/java/com/servlet/AddUser.java
@@ -2,29 +2,17 @@ package main.java.com.servlet;
 
 import main.java.com.DAO.UserDAO;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
 public class AddUser extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public AddUser() {
-        System.out.println("AddUser Constructor called!");
-    }
-
-    public void init(ServletConfig config) throws ServletException {
-        System.out.println("AddUser \"Init\" method called");
-    }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FirstServlet \"DoPost\" method called");
         doPostAndRespond(request, response);
     }
 
@@ -34,9 +22,5 @@ public class AddUser extends HttpServlet {
         String password = request.getParameter("user_password");
 
         new UserDAO().SignUp(name, email, password);
-
-        PrintWriter out = response.getWriter();
-        out.write("<html><body><h2>Username and email id is stored in the session, go back and click on \"TestSession\" to test the session</h2></body></html>");
-
     }
 }
